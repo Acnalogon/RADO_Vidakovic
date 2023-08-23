a34982988e808e1459627a4d4085e1f2541a2945
Acnalogon <rado.work21@gmail.com>
2023-08-23 02:29:48 +0200

    23/08/23

--- animation.js
+++ animation.js
@@ -1,19 +1,14 @@
-/* !
-    * fullPage 4.0.10
-    * https://github.com/alvarotrigo/fullPage.js
-    *
-    * @license GPLv3 for open source use only
-    * or Fullpage Commercial License for commercial use
-    * http://alvarotrigo.com/fullPage/pricing/
-    *
-    * Copyright (C) 2021 http://alvarotrigo.com/fullPage - A project by Alvaro Trigo
-*/
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        // FullPage.js options here
+      licenseKey: 'gplv3-license',
+      navigation: false,
+      navigationPosition: 'none',
+      controlArrows: true,
+      scrollingSpeed: 1000,
+      anchors: ['About_Me', 'My_Skills', 'Contact_Me', 'Front_End', 'Back_End', 'Programming_Languages'],
 
-new fullpage('#fullpage', {
-  licenseKey: 'gplv3-license',
-  navigation: false,
-  navigationPosition: 'none',
-  controlArrows: true,
-  scrollingSpeed: 1000,
-  anchors: ['About_Me', 'My_Skills', 'Contact_Me', 'Front_End', 'Back_End', 'Programming_Languages'],
+      slidesNavigation: true,
+	    slidesNavPosition: 'bottom',
+    });
 });
