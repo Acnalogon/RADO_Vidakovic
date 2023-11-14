51ea8b3e7ea324e16343cc3d379a32cf97df687d
Acnalogon <rado.work21@gmail.com>
2023-11-14 21:43:36 +0100

    close to final design

--- animation.js
+++ animation.js
@@ -1,60 +1,74 @@
-$(document).ready(function () {
-    $('#fullpage').fullpage({
-        // FullPage.js options here
-      licenseKey: 'gplv3-license',
-      navigation: false,
-      navigationPosition: 'none',
-      controlArrows: true,
-      scrollingSpeed: 1000,
-      anchors: ['About_Me', 'My_Skills', 'Contact_Me', 'Front_End', 'Back_End', 'Programming_Languages'],
+console.clear();
 
-      slidesNavigation: true,
-	    slidesNavPosition: 'bottom',
-    });
+$(document).ready(function () {
+  $("#fullpage").fullpage({
+    // FullPage.js options here
+    licenseKey: "gplv3-license",
+    navigation: false,
+    navigationPosition: "none",
+    controlArrows: true,
+    scrollingSpeed: 1000,
+    anchors: [
+      "Home",
+      "About_Me",
+      "My_Skills",
+      "Contact_Me",
+      "Front_End",
+      "Back_End",
+      "Programming_Languages",
+    ],
+    menu: "#myMenu",
+    slidesNavigation: true,
+    slidesNavPosition: "bottom",
+  });
 });
 
-/* const animation = anima({
-            targets: '.mandala-container',
-            transform: ['rotate(0deg)', 'rotate(360deg)'],
-            easing: 'linear',
-            duration: 500000, // 500s
-            loop: true,
-        });
+const app = (() => {
+  let body;
+  let menu;
+  let menuItems;
 
-        // Start the animation
-        animation.play(); */
+  const init = () => {
+    body = document.querySelector("body");
+    menu = document.querySelector(".menu-icon");
+    menuItems = document.querySelectorAll(".nav__list-item");
 
-VANTA.HALO({
+    applyListeners();
+  };
+
+  const applyListeners = () => {
+    menu.addEventListener("click", () => toggleClass(body, "nav-active"));
+
+    // Add an event listener to each menu item
+    menuItems.forEach((item) => {
+      item.addEventListener("click", () => {
+        // Assuming 'nav-active' is the class that shows the menu
+        if (body.classList.contains("nav-active")) {
+          toggleClass(body, "nav-active");
+        }
+      });
+    });
+  };
+
+  const toggleClass = (element, stringClass) => {
+    if (element.classList.contains(stringClass)) {
+      element.classList.remove(stringClass);
+    } else {
+      element.classList.add(stringClass);
+    }
+  };
+
+  init();
+})();
+
+VANTA.DOTS({
   el: "#vanta_back",
   mouseControls: true,
   touchControls: true,
   gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00,
-  backgroundColor: 0x0
-})
-
-
-
-$(document).ready(function(){
-    $('[data-bs-toggle="offcanvas"]').on('click', function () {
-      $('.offcanvas-collapse').toggleClass('open');
-    });
-});
-  
-const aboutLink = document.getElementById('aboutLink');
-const skillsLink = document.getElementById('skillz');
-const contactLink = document.getElementById('contactLink');
-const offcanvas = new bootstrap.Offcanvas(document.getElementById('offcanvasDarkNavbar'));
-
-aboutLink.addEventListener('click', () => {
-    offcanvas.hide();
-});
-
-skillsLink.addEventListener('click', () => {
-    offcanvas.hide();
-});
-
-contactLink.addEventListener('click', () => {
-    offcanvas.hide();
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  showLines: false,
 });
