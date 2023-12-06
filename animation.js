1e6721714192647a65d6952b6622a7860e27b731
Acnalogon <rado.work21@gmail.com>
2023-12-06 23:20:34 +0100

    patch 0.9

--- animation.js
+++ animation.js
@@ -1,32 +1,11 @@
+//navigation menu code ------------------
 console.clear();
-
-$(document).ready(function () {
-  $("#fullpage").fullpage({
-    // FullPage.js options here
-    licenseKey: "gplv3-license",
-    navigation: false,
-    navigationPosition: "none",
-    controlArrows: true,
-    scrollingSpeed: 1000,
-    anchors: [
-      "Home",
-      "About_Me",
-      "My_Skills",
-      "Contact_Me",
-      "Front_End",
-      "Back_End",
-      "Programming_Languages",
-    ],
-    menu: "#myMenu",
-    slidesNavigation: true,
-    slidesNavPosition: "bottom",
-  });
-});
-
 const app = (() => {
   let body;
   let menu;
   let menuItems;
+  let nav;
+  let site;
 
   const init = () => {
     body = document.querySelector("body");
@@ -60,7 +39,71 @@ const app = (() => {
 
   init();
 })();
+/* 
+//viewport detection--------
+document.addEventListener("scroll", function () {
+  var article = document.querySelector("article");
+  var articlePosition = article.getBoundingClientRect();
 
+  // Check if article is in the viewport
+  if (articlePosition.top < window.innerHeight && articlePosition.bottom >= 0) {
+    article.style.opacity = "1";
+    article.style.transform = "translateX(0)";
+    article.style.visibility = "visible";
+  }
+}); */
+
+//fullpage.js animation code -----------
+$(document).ready(function () {
+  $("#fullpage").fullpage({
+    // FullPage.js options
+    licenseKey: "gplv3-license",
+    navigation: false,
+    navigationPosition: "none",
+    controlArrows: true,
+    scrollingSpeed: 1000,
+    anchors: [
+      "Home",
+      "About_Me",
+      "My_Skills",
+      "Contact_Me",
+      "Front_End",
+      "Back_End",
+      "Programming_Languages",
+    ],
+    menu: "#myMenu",
+    slidesNavigation: true,
+    slidesNavPosition: "bottom",
+
+    afterLoad: function (origin, destination, direction) {
+      // Handle section loading
+      var loadedSection = destination.item;
+      var articleInSection = loadedSection.querySelector("article");
+      if (articleInSection) {
+        animateArticle(articleInSection);
+      }
+    },
+
+    afterSlideLoad: function (section, origin, destination, direction) {
+      // Handle slide loading within a section
+      var loadedSlide = destination.item;
+      var articleInSlide = loadedSlide.querySelector("article");
+      if (articleInSlide) {
+        animateArticle(articleInSlide);
+      }
+    },
+  });
+});
+
+function animateArticle(article) {
+  article.style.opacity = "1";
+  article.style.transform = "translateX(0)";
+  article.style.visibility = "visible";
+  article.style.transition =
+    "opacity 0.5s ease-in-out, transform 0.5s ease-in-out";
+}
+
+//VANTA.js animation code ---------------
 VANTA.DOTS({
   el: "#vanta_back",
   mouseControls: true,
@@ -70,5 +113,6 @@ VANTA.DOTS({
   minWidth: 200.0,
   scale: 1.0,
   scaleMobile: 1.0,
+  color: 0xffffff,
   showLines: false,
 });
